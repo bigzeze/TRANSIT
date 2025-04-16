345b096ee5d3ce03ff8866384dd1ee94ae931bd7
bigzeze <965511871@qq.com>
2025-04-16 16:46:17 +0800

    addconfig

--- src/run.py
+++ src/run.py
@@ -1,36 +1,9 @@
-import TRANSIT
+from TRANSIT import TRANSIT
+import configparser
 
 if __name__ == '__main__':
-    transit = TRANSIT()
-    transit.parse_args()
-    transit.check_paths()
-    transit.apply_simulation()
-
-    
-
-
-
-    myconfig = SimulationConfig()
-    gridNumber = 5
-    detectorNumber = 1
-    laneLength = 300
-    myconfig.setNetworkConfig(True,gridNumber,laneLength,1)
-    myconfig.setDetectorConfig(60,'center',detector_number=detectorNumber)
-    myconfig.setFlowConfig(0.2,0.5,0.4)
-    myconfig.setSimulationTime(0,14400)
-
-    num_ES = 1
-    #num_VS = 3
-    num_TLF = 0
-
-    net = get_crossing_name(gridNumber)
-
-    inject_edges_ES = [random_edge(net) for i in range(num_ES)] # EdgeSlow positions
-    #inject_edges_VS = [random_edge(net) for i in range(num_VS)] # VehicleStop positions
-    inject_crossings_TLF = [random_crossing(net) for i in range(num_TLF)] # TLFailure positions
-    if mode == 'observe':
-        print('inject area: '+ ' '.join(inject_edges_ES) + ' ' + ' '.join(inject_crossings_TLF))
-
-
-    start_time = 3600
-    end_time = 7200
+    app = TRANSIT()
+    config = configparser.ConfigParser()
+    app.set_args(**configure)
+    app.check_paths()
+    app.apply_simulation()
--- test.py
+++
@@ -1,6 +0,0 @@
-orgspeed = 13.89
-s0 = 7.5
-beta = 0.1
-tr = 1
-newspeed = (orgspeed*s0*beta/(s0+tr*orgspeed))/(1-orgspeed*tr*beta/(s0+tr*orgspeed))
-print(newspeed)
